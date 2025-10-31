eaedd30532d5f9e91c27057eb55544efd1a9b1ff
Muskan Gupta <115357215+MuskanNITA@users.noreply.github.com>
2025-10-31 18:26:36 +0530

    Update api.js
    Updated backend connection (using render backend )

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: "https://slotswapper-7y9v.onrender.com"
 });
 
 export function setAuthToken(token) {
